3789083c4d3139ecfb38ab7b236f3bf220f0f479
David Haifley <dhaifley@gmail.com>
2020-02-26 19:08:16 -0500

    Add unit tests

--- spect_test.go
+++ spect_test.go
@@ -29,3 +29,149 @@ func TestRequestString(t *testing.T) {
 		t.Errorf("Expected string: %v, got: %v", exp, a.String())
 	}
 }
+
+func TestRequestEqual(t *testing.T) {
+	a := &Request{
+		URL:    "test.com",
+		Method: "GET",
+		Body:   "test",
+	}
+
+	b := &Request{
+		URL:    "test.com",
+		Method: "GET",
+		Body:   "test",
+	}
+
+	if !a.Equal(b) {
+		t.Error("Expected equal: true, got: false")
+	}
+
+	b.Body = "error"
+	if a.Equal(b) {
+		t.Error("Expected equal: false, got: true")
+	}
+
+	b.Method = "error"
+	if a.Equal(b) {
+		t.Error("Expected equal: false, got: true")
+	}
+
+	b.URL = "error"
+	if a.Equal(b) {
+		t.Error("Expected equal: false, got: true")
+	}
+
+	b = nil
+	if a.Equal(b) {
+		t.Error("Expected equal: false, got: true")
+	}
+}
+
+func TestResponseString(t *testing.T) {
+	a := &Response{
+		Code: 200,
+		Body: "test",
+	}
+
+	exp := `{"code":200,"body":"test"}` + "\n"
+	if a.String() != exp {
+		t.Errorf("Expected string: %v, got: %v", exp, a.String())
+	}
+}
+
+func TestResponseEqual(t *testing.T) {
+	a := &Response{
+		Code: 200,
+		Body: "test",
+	}
+
+	b := &Response{
+		Code: 200,
+		Body: "test",
+	}
+
+	if !a.Equal(b) {
+		t.Error("Expected equal: true, got: false")
+	}
+
+	b.Body = "error"
+	if a.Equal(b) {
+		t.Error("Expected equal: false, got: true")
+	}
+
+	b.Code = 500
+	if a.Equal(b) {
+		t.Error("Expected equal: false, got: true")
+	}
+
+	b = nil
+	if a.Equal(b) {
+		t.Error("Expected equal: false, got: true")
+	}
+}
+
+func TestSpecTestString(t *testing.T) {
+	a := &SpecTest{
+		Req: &Request{
+			URL:    "test.com",
+			Method: "GET",
+			Body:   "test",
+		},
+		Res: &Response{
+			Code: 200,
+			Body: "test",
+		},
+	}
+
+	exp := `{"req":{"url":"test.com","method":"GET","body":"test"},"res":` +
+		`{"code":200,"body":"test"}}` + "\n"
+	if a.String() != exp {
+		t.Errorf("Expected string: %v, got: %v", exp, a.String())
+	}
+}
+
+func TestSpecTestEqual(t *testing.T) {
+	a := &SpecTest{
+		Req: &Request{
+			URL:    "test.com",
+			Method: "GET",
+			Body:   "test",
+		},
+		Res: &Response{
+			Code: 200,
+			Body: "test",
+		},
+	}
+
+	b := &SpecTest{
+		Req: &Request{
+			URL:    "test.com",
+			Method: "GET",
+			Body:   "test",
+		},
+		Res: &Response{
+			Code: 200,
+			Body: "test",
+		},
+	}
+
+	if !a.Equal(b) {
+		t.Error("Expected equal: true, got: false")
+	}
+
+	b.Res.Body = "error"
+	if a.Equal(b) {
+		t.Error("Expected equal: false, got: true")
+	}
+
+	b.Req.Body = "error"
+	if a.Equal(b) {
+		t.Error("Expected equal: false, got: true")
+	}
+
+	b = nil
+	if a.Equal(b) {
+		t.Error("Expected equal: false, got: true")
+	}
+}
