7a0ab80544492fce55b33a61a2a5c269dbe744fa
David Haifley <dhaifley@gmail.com>
2020-02-28 00:54:47 -0500

    Parse examples

--- parser.go
+++ parser.go
@@ -15,24 +15,113 @@ package spect
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 
 	"gopkg.in/yaml.v3"
 )
 
-func ParseOpenAPI() (map[string]interface{}, error) {
-	data := `
-example:
-    a: 1
-    b: "test"
-`
+// Example values are used to capture information about a spec example.
+type Example struct {
+	Name    string
+	Type    string
+	Levels  []string
+	Example interface{}
+}
 
+// String returns the value as a JSON formatted string.
+func (e *Example) String() string {
+	b := &bytes.Buffer{}
+	if err := json.NewEncoder(b).Encode(e); err != nil {
+		return "ERROR unable to encode value"
+	}
+
+	return b.String()
+
+}
+
+// ParseArray recusively parses a YAML map finding examples.
+func ParseArray(a []interface{}, lvl []string,
+	exs []*Example, name, extype string) ([]*Example, error) {
+	for _, v := range a {
+		var err error
+		switch vv := v.(type) {
+		case []interface{}:
+			exs, err = ParseArray(vv, lvl, exs, name, extype)
+		case map[string]interface{}:
+			exs, err = ParseMap(vv, lvl, exs, name, extype)
+		}
+
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return exs, nil
+}
+
+// ParseMap recusively parses a YAML map finding examples.
+func ParseMap(m map[string]interface{}, lvl []string,
+	exs []*Example, name, extype string) ([]*Example, error) {
+	ex := &Example{
+		Name:   name,
+		Type:   extype,
+		Levels: lvl,
+	}
+
+	for k, v := range m {
+		switch k {
+		case "name":
+			if n, ok := v.(string); ok {
+				name = n
+				ex.Name = n
+			}
+		case "type":
+			if t, ok := v.(string); ok {
+				extype = t
+				ex.Type = t
+			}
+		}
+	}
+
+	for k, v := range m {
+		switch k {
+		case "example":
+			ex.Example = v
+		}
+
+		var err error
+		switch vv := v.(type) {
+		case []interface{}:
+			exs, err = ParseArray(vv, append(lvl, k), exs, name, extype)
+		case map[string]interface{}:
+			exs, err = ParseMap(vv, append(lvl, k), exs, name, extype)
+		}
+
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if len(lvl) > 2 && lvl[len(lvl)-2] == "properties" {
+		ex.Name = lvl[len(lvl)-1]
+	}
+
+	if ex.Example != nil {
+		return append(exs, ex), nil
+	}
+
+	return exs, nil
+}
+
+// ParseOpenAPI parses OpenAPI YAML specification data into a slice of
+// SpecTest values.
+func ParseOpenAPI(data string) ([]*Example, error) {
 	m := make(map[string]interface{}, 0)
 	err := yaml.NewDecoder(bytes.NewBufferString(data)).Decode(&m)
 	if err != nil {
 		return nil, fmt.Errorf("unable to parse yaml: %w", err)
 	}
 
-	fmt.Println(m)
-	return m, nil
+	return ParseMap(m, []string{}, []*Example{}, "", "")
 }
--- parser_test.go
+++ parser_test.go
@@ -17,11 +17,171 @@ import (
 	"testing"
 )
 
+const testSpecData = `
+openapi: "3.0.2"
+info:
+  title: "Circonus Application Programming Interface"
+  version: "0.6.1"
+servers:
+  - url: "https://api.circonus.com"
+paths:
+  /broker:
+    get:
+      tags:
+        - Broker
+      summary: Retrieves broker configuration details for the current account.
+      responses:
+        "200":
+         content:
+            application/json:
+              schema:
+                type: array
+                items:
+                  $ref: "#/components/schemas/Broker"
+        "400":
+          $ref: "#/components/responses/ErrorBadRequest"
+        "401":
+          $ref: "#/components/responses/ErrorUnauthorized"
+        "403":
+          $ref: "#/components/responses/ErrorForbidden"
+        "404":
+          $ref: "#/components/responses/ErrorNotFound"
+        "5XX":
+          $ref: "#/components/responses/ErrorInternal"
+  /broker/{id}:
+    get:
+      tags:
+        - Broker
+      summary: Retrieves details for the specified broker by ID.
+      description: >
+        Retrieves the configuration details for a broker specified by the ID
+        in the request path.
+      parameters:
+        - in: path
+          name: id
+          description: >
+            The broker ID of the configuration details to retrieve.
+          required: true
+          schema:
+            type: integer
+            format: int64
+            example: 1
+      responses:
+        "200":
+          description: Broker configuration information.
+          content:
+            application/json:
+              schema:
+                $ref: "#/components/schemas/Broker"
+        "400":
+          $ref: "#/components/responses/ErrorBadRequest"
+        "401":
+          $ref: "#/components/responses/ErrorUnauthorized"
+        "403":
+          $ref: "#/components/responses/ErrorForbidden"
+        "404":
+          $ref: "#/components/responses/ErrorNotFound"
+        "5XX":
+          $ref: "#/components/responses/ErrorInternal"
+components:
+  securitySchemes:
+    App-Name:
+      in: header
+      name: X-Circonus-App-Name
+      type: apiKey
+    Auth-Token:
+      in: header
+      name: X-Circonus-Auth-Token
+      type: apiKey
+  schemas:
+    Tag:
+      type: string
+      description: >
+        An associated tag.
+        A tag is just a string, with or without a colon, such as 'foo', 'bar',
+        'datacenter:london', or 'os:linux'. The part of the string before the
+        colon is considered the category the tag is in; Tag strings without a
+        colon will place the string in the 'uncategorized' category. Circonus
+        will lowercase the contents of the string before storing it.
+      example: datacenter:primary
+    Broker:
+      type: object
+      properties:
+        _cid:
+          type: string
+          description: >
+            The primary identifier of a broker configuration.
+            A string containing a broker cid.
+          example: /broker/1
+        _details:
+          type: array
+          description: >
+            An array of details on every broker that is grouped together.
+          example:
+            [
+              {
+                "cn": "us1.noit.circonus.net",
+                "external_host": "example.circonus.net",
+                "external_port": 8080,
+                "ipaddress": "75.102.43.87",
+                "minimum_version_required": 1367937537,
+                "modules": ["cim", "circonuswindowsagent", "collectd", "dcm"],
+                "port": null,
+                "skew": "0.00257706642150879",
+                "status": null,
+                "version": 1370978917,
+              },
+            ]
+          items:
+            $ref: "#/components/schemas/BrokerDetails"
+        _latitude:
+          type: number
+          format: double
+          description: >
+            The latitude of the broker.
+            A floating point number indicating GPS location.
+          example: "39.043"
+        _longitude:
+          type: number
+          format: double
+          description: >
+            The longitude of the broker.
+            A floating point number indicating GPS location.
+          example: "-77.487"
+        _name:
+          type: string
+          description: >
+            The name of the broker.
+            A string containing freeform text.
+          example: Ashburn, VA, US
+        _tags:
+          type: array
+          description: >
+            The tags associated with this broker.
+            An array of tags. The tags in the array are automatically sorted,
+            de-duplicated and transformed into their lowercase canonical form.
+          example: ["datacenter:primary", "foo:bar"]
+          items:
+            $ref: "#/components/schemas/Tag"
+        _type:
+          type: string
+          description: >
+            The type of broker, whether public or private.
+            A string containing either 'circonus' (a cloud based broker run by
+            Circonus) or 'enterprise' (an in-house enterprise broker).
+          example: circonus
+tags:
+  - name: Broker
+    description: >
+      The Broker service provides access to configuration information about
+      brokers servers.
+`
+
 func TestParseOpenAPI(t *testing.T) {
-	m, err := ParseOpenAPI()
+	tests, err := ParseOpenAPI(testSpecData)
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 
-	t.Error(m)
+	t.Errorf("%+v", tests)
 }
